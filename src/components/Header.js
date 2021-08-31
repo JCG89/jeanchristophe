d002a4c6ad3491c91722a44fce1c6ecf89d7789b
Jean Christophe Gomis <devdujour@yahoo.com>
2021-08-31 10:54:04 +0200

    mise en place du header ,installation des librairie type et particles et stylisation du header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Typed from "react-typed"
+
+export default function Header() {
+      return (
+            <div className="header-wrapper">
+                  <div className="main-info">
+                        <h1> Jean Christophe Gomis </h1>
+                        <Typed
+                              className="typed-text"
+                              strings={["Développeur web &web mobile",
+                                    "HTML5 CSS3 JS PHP", "REACT SYMFONY 5 WORDPRESS"]}
+                              typeSpeed={40}
+                              backSpeed={60}
+                              loop
+                        />
+                  </div>
+            </div>
+      )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
 
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                   <div className="container">
-                        <a className="navbar-brand" href="#">Jean Christophe Gomis</a>
+                        <a className="navbar-brand" href="#">J C G</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                               <span className="navbar-toggler-icon"><img src={menu} style={{ width: "50px", color: "white" }} /></span>
                         </button>
