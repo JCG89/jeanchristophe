f19bb7bee5aeafb935ccf89a541b1d5caf74df05
Jean Christophe Gomis <devdujour@yahoo.com>
2021-09-02 11:42:22 +0200

    Re-structuration du composant experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,44 +1,96 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarCheck, faIdBadge } from '@fortawesome/free-solid-svg-icons'
 
 export default function Experience() {
       return (
-            <div className="experience">
-                  <div className="d-flex justify-content-center my-5">
-                        <h1> Experience</h1>
-                  </div>
-                  <div className="container experience-wrapper">
-                        <div className="timeline-block timeline-block-right">
-                              <div className="marker"></div>
-                              <div className="timeline-content">
-                                    <h3>2003 - 20014</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio non consectetur dolor expedita inventore rerum, saepe quo possimus aut nisi!</p>
-                              </div>
-                        </div>
-                        <div className="timeline-block timeline-block-left">
-                              <div className="marker"></div>
-                              <div className="timeline-content">
-                                    <h3>2003 - 20014</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio non consectetur dolor expedita inventore rerum, saepe quo possimus aut nisi!</p>
-                              </div>
-                        </div>
-                        {/*-*/}
-                        <div className="timeline-block timeline-block-right">
-                              <div className="marker"></div>
-                              <div className="timeline-content">
-                                    <h3>2003 - 20014</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio non consectetur dolor expedita inventore rerum, saepe quo possimus aut nisi!</p>
-                              </div>
-                        </div>
-                        {/*-*/}
-                        <div className="timeline-block timeline-block-left">
-                              <div className="marker"></div>
-                              <div className="timeline-content">
-                                    <h3>2003 - 20014</h3>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio non consectetur dolor expedita inventore rerum, saepe quo possimus aut nisi!</p>
-                              </div>
-                        </div>
-                  </div>
+            <>
 
-            </div>
+                  <section className="experience">
+                        <div className="container">
+                              <div className="white-divider"></div>
+                              <div className="heading">
+                                    <h2>Expéreince professionelle</h2>
+                              </div>
+                              <ul className="timeline">
+                                    <li>
+                                          <div className="timeline-badge"><span className="iconify" data-icon="octicon:briefcase" data-inline="false"><FontAwesomeIcon icon={faIdBadge} size="xs" /> </span></div>
+                                          <div className="timeline-panel-container">
+                                                <div className="timeline-panel">
+                                                      <div className="timeline-heading">
+                                                            <h3>Magic Rambo</h3>
+                                                            <h4>Operateur de Production</h4>
+                                                            <p className="text-muted"><small className="far fa-calendar-alt"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small>2019-2020</p>
+                                                      </div>
+                                                      <div className="timeline-body">
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </li>
+                                    {/*-*/}
+                                    <li>
+                                          <div className="timeline-badge"><span className="iconify" data-icon="octicon:briefcase" data-inline="false"><FontAwesomeIcon icon={faIdBadge} size="xs" /></span></div>
+                                          <div className="timeline-panel-container-inverted">
+                                                <div className="timeline-panel">
+                                                      <div className="timeline-heading">
+                                                            <h3>Magic Rambo</h3>
+                                                            <h4>Operateur de Production</h4>
+                                                            <p className="text-muted"><small className="far fa-calendar-alt"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020</p>
+                                                      </div>
+                                                      <div className="timeline-body">
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </li>
+                                    {/*-*/}
+                                    <li>
+                                          <div className="timeline-badge"><span className="iconify" data-icon="octicon:briefcase" data-inline="false"><FontAwesomeIcon icon={faIdBadge} size="xs" /></span></div>
+                                          <div className="timeline-panel-container">
+                                                <div className="timeline-panel">
+                                                      <div className="timeline-heading">
+                                                            <h3>Magic Rambo</h3>
+                                                            <h4>Operateur de Production</h4>
+                                                            <p className="text-muted"><small className="far fa-calendar-alt"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small>2019-2020</p>
+                                                      </div>
+                                                      <div className="timeline-body">
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </li>
+                                    {/*-*/}
+                                    <li>
+                                          <div className="timeline-badge"><span className="iconify" data-icon="octicon:briefcase" data-inline="false"></span><FontAwesomeIcon icon={faIdBadge} size="xs" /></div>
+                                          <div className="timeline-panel-container-inverted">
+                                                <div className="timeline-panel">
+                                                      <div className="timeline-heading">
+                                                            <h3>Magic Rambo</h3>
+                                                            <h4>Operateur de Production</h4>
+                                                            <p className="text-muted"><small className="far fa-calendar-alt"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small>2019-2020</p>
+                                                      </div>
+                                                      <div className="timeline-body">
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                            <p><small className="far fa-calendar-check"><FontAwesomeIcon icon={faCalendarCheck} size="xs" /></small> 2019-2020 Gestion de stock et de la logistique</p>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </li>
+
+                              </ul>
+                        </div>
+
+                  </section>
+
+
+            </>
       )
 }
