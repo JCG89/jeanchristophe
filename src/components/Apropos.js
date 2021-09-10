2345de85ab022a92cd00bd2a9cc78358c291d76a
Jean Christophe Gomis <devdujour@yahoo.com>
2021-09-11 00:22:59 +0200

    modification du composant apropos

--- src/components/Apropos.js
+++ src/components/Apropos.js
@@ -16,24 +16,17 @@ export default function Apropos() {
                               <div className="col-lg-6 col-xm-12">
                                     <h1 className="apropos"> A propos de moi</h1>
                                     <p className="parag">
-                                          Je m'appelle Jean Christophe Gomis, je suis titulaire d'un DUT en développement web et web mobile classé au RNCP Niveau V .Issu d’un parcours sport-études au Sénégal, où j’ai fais
-                                          mes premiers pas dans la technologie. J’ai ensuite
-                                          embrassé une carrière de footballeur dans la LSFP (ligue
-                                          sénégalaise de football professionnel) . Arrivé en France
-                                          en 2017 , j’ai occupé plusieurs postes , dans un premier
-                                          temps en tant que vendeur-gérant à VIVAL By CASINO
-                                          2017 - 2019 , puis en tant qu’opérateur de production à
-                                          MAGIC RAMBO (sous-traitant Disneyland Paris) 2019
-                                          -2020 .Depuis Février 2020 , j’ai entamé une reconversion
-                                          professionnelle en tant que développeur web .
-                                          J'ai suivi une formation à l'AFPA de Créteil où j’ai valider
-                                          un titre professionnel de développeur web et web mobile
-                                          RNCP de niveau 5 (bac+ 2). J’ai ensuite décider de
-                                          poursuivre avec OpenClassrooms en alternance pour
-                                          concrétiser mes ambitions. Minutieux, ,sérieux ,et créatif,
-                                          j’aime le travail d’équipe et apprendre de l’autre. Je
-                                          cherche une entreprise ambitieuse, dans laquelle placer
-                                          toute mon énergie.
+                                          Je m'appelle Jean Christophe Gomis, je suis  titulaire d'un DUT en développement web et web mobile classé au RNCP Niveau V . Issu d’un parcours sport-études au Sénégal, où j’ai fait mes premiers pas dans la technologie  et la physique chimie au collège d’enseignement   technique  Amilcar Cabral de Ziguinchor. Après mon brevet, j'ai intégré le lycée technique  Maurice Delafosse de Dakar. En parallèle de mes études, j'ai intégré le centre de formation de L’As Douane  avant d'embrasser une carrière de footballeur dans la LSFP (ligue sénégalaise de football professionnel) . Arrivé en France en 2017 , j’ai occupé plusieurs postes , dans un premier temps en tant que vendeur-gérant à VIVAL By CASINO 2017 - 2019 , puis en tant qu’opérateur de production à MAGIC RAMBO (sous-traitant Disneyland Paris) 2019 -2020 . Je suis un passionné de la tech  depuis toujours et particulièrement de la programmation informatique. Après plusieurs tests concluants , j’ai intégré l’Afpa de Créteil en septembre 2019 pour une formation en bureautique, avant d'enchaîner en   Février 2020  avec  le développement informatique. Je suis à l’écoute du marché  et je suis ouvert à toute collaboration ambitieuse.
+                                          Je suis inscrit à l’une des meilleurs institut supérieur en ligne qu’est OpenClassrooms pour une formation en alternance dans l’optique de valider un titre de niveau six (Bac+¾) car le numérique est en constante évolution et que l’apprentissage ne s'arrête jamais .
+                                          Je suis aussi auto-entrepreneur dans la tech avec quelques projets à mon actif que vous pouvez retrouver dans mon portfolio.
+                                          Bonne visite de mon site à toutes et à tous et n’hésitez pas à me contacter via le formulaire  pour envisager des choses ensemble.
+
+                                          Bien cordialement
+                                          <br />
+                                          <b>Jean Christophe gomis</b>
+                                          <br />
+                                          Développeur web et web mobile
+
 
                                     </p>
                               </div>
