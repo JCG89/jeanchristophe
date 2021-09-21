3b4045576f5e50b94b5175f1c243f5a7d513234c
Jean Christophe Gomis <devdujour@yahoo.com>
2021-09-21 12:51:01 +0200

    modification des photos dans le composant portfolio et dans le fichier App.css

--- src/components/PortfolioCarousel.js
+++ src/components/PortfolioCarousel.js
@@ -11,28 +11,33 @@ const PortfolioCarousel = () => {
       return (
             <Carousel className="carousel">
                   <>
-                        <div className="jc">
+                        <div>
+                              <img src={avatar1} alt=" projet-symfony" className="portfolio-tof" />
+                              <h3 className="jc"><a href="https://github.com/christophegomis89/awara-boutique" id="jc">AWARA-BOUTIQUE</a></h3>
+                              <p className="text">Site e-commerce avec le framework Symfony 5 :Préenté au jury le 10/06/2021 titre professionnel de développeur web et web mobile RNCP NIVEAU 5</p>
                         </div>
-                        <img src={avatar1} alt=" projet-symfony" />
-                        <h3><a href="https://github.com/christophegomis89/awara-boutique" id="jc">AWARA-BOUTIQUE</a></h3>
-                        <p className="text">Site e-commerce avec le framework Symfony 5 :Préenté au jury le 10/06/2021 titre professionnel de développeur web et web mobile RNCP NIVEAU 5</p>
-                  </>
-                  <>
-                        <img src={avatar2} alt="mon portfolio" className="portfolio-tof" />
-                        <h3> <a href="https://github.com/christophegomis89/cv-jean-christophe-gomis" id="jc">MON PORTFOLIO</a> </h3>
-                        <p className="text"> Mon premier portfolio ,réalisé avec le framework REACT JS </p>
-                  </>
-                  <>
-                        <div className="jc">
-                              <img src={avatar3} alt=" projet-react " className="portfolio-tof" />
-                              <h3> <a href="https://gsportnews.fr" id="jc">Gsportnews.fr</a></h3>
-                              <p className="text"> Projet réalisé avec REACT JS. Ce nom de domaine est dédié à mon blog  d'information sportif , mais heberge temporairement mon portfolio qui deviendra Jean Christophe.com ou .fr</p></div>
 
                   </>
+
                   <>
-                        <div className="jc">
-                              <img src={avatar4} alt=" smblsemedo" className="portfolio-tof" />
-                              <h3> <a href="https://smblsemedo.com" id="jc">site vitrine smbl semedo</a></h3>
+                        <div>
+                              <img src={avatar2} alt="mon portfolio" className="portfolio-tof" />
+                              <h3 className="jc"> <a href="https://github.com/christophegomis89/cv-jean-christophe-gomis" id="jc">MON PORTFOLIO</a> </h3>
+                              <p className="text"> Mon premier portfolio ,réalisé avec le framework REACT JS </p>
+                        </div>
+                  </>
+                  <>
+                        <div>
+                              <img src={avatar3} alt="projet-react" className="portfolio-tof" />
+                              <h3 className="jc"> <a href="https://gsportnews.fr" id="jc">Gsportnews.fr</a></h3>
+                              <p className="text"> Projet réalisé avec REACT JS. Ce nom de domaine est dédié à mon blog  d'information sportif , mais heberge temporairement mon portfolio qui deviendra Jean Christophe.com ou .fr</p>
+
+                        </div>
+                  </>
+                  <>
+                        <div>
+                              <img src={avatar4} alt="smblsemedo" className="portfolio-tof" />
+                              <h3 className="jc"> <a href="https://smblsemedo.com" id="jc">site vitrine smbl semedo</a></h3>
                               <p className="text"> Projet site vitrine  de l'entreprise du BTP SMBLSEMEDO réalisé avec le CMS WORDPRESS </p>
                         </div>
 
